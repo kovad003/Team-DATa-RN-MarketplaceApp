09d23b6c4a02fb6758a6a3cc951bf68c05778770
Coder-AD-3000 <78084889+Coder-AD-3000@users.noreply.github.com>
2021-10-04 23:35:34 +0300

    Update CreateItemScreen.js

--- src/screens/CreateItemScreen.js
+++ src/screens/CreateItemScreen.js
@@ -27,41 +27,51 @@ function CreateItemScreen(props) {
 const styles = StyleSheet.create({
   containerCIS: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: 'center', // AD - added align
   },
   headerCIS: {
     height: 39,
     width: 339,
     marginTop: 35,
-    marginLeft: 10
+    //marginLeft: 10 // AD - removed
+    alignItems: 'center',
+    backgroundColor: 'red', // AD - just for testing
   },
   imageUploaderCIS: {
     height: 104,
     width: 339,
     marginTop: 21,
     marginLeft: 10,
-    margin: Margins.midsize, // AD - added
-    padding: Paddings.midsize, // AD - added
+    //margin: Margins.midsize, // AD - removed
+    //padding: Paddings.midsize, // AD - added
+
+    //alignItems: 'center',
   },
   iconInputBoxAttachedCIS: {
     height: 44,
     width: 317,
     marginTop: 42,
     marginLeft: 20,
-    padding: Paddings.midsize, // AD - added
+    //padding: Paddings.midsize, // AD - removed
+
+    alignItems: 'center', // AD - added align
   },
   buttonContainerCIS: {
-    width: 339,
+    //width: 339, AD - removed
     height: 89,
     marginTop: 250,
-    marginLeft: 10,
-    padding: Paddings.midsize, // AD - added
+    //marginLeft: 10, // AD - removed
+    //padding: Paddings.midsize, // AD - removed
+    width: '80%',
+
+    alignItems: 'center', // AD - added align
+    backgroundColor: 'red', // AD - just for testing
   },
   uploadButtonCIS: {
     width: 206,
     height: 36,
     marginTop: 4,
-    marginLeft: 66
+    // marginLeft: 66 // AD - removed
   },
   cancelButtonCIS: {
     width: 206,
@@ -69,7 +79,7 @@ const styles = StyleSheet.create({
     overflow: "visible",
     backgroundColor: "rgba(11,67,130,1)",
     marginTop: 9,
-    marginLeft: 66
+    // marginLeft: 66 // AD - removed
   }
 });
 
