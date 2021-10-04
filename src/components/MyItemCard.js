d208dff98edfaedd61f42da39d511ea708e627c9
Hossein Hazratgholizadeh <hossein.hazratgholizade@gmail.com>
2021-10-05 01:52:30 +0300

    Create MyItemCard component and Fix the ListItemScreen
    Create MyItemCard component and Fix the ListItemScreen to work properly with data.
    also, add some styling to MyItemCard  and ListItemScreen .
    in addition add some dummy-data for items.

---
+++ src/components/MyItemCard.js
@@ -0,0 +1,126 @@
+import React from "react";
+import {View, Text, StyleSheet,FlatList, Button, ImageBackground , TouchableOpacity, Platform, TouchableNativeFeedback} from 'react-native';
+import IconButton from "./IconButton";
+
+
+
+
+const MyItemCard = props => {
+
+return (
+    <View style={styles.postedItem}>
+    <TouchableOpacity onPress={props.onSelectPost} >
+        <View>
+            <View style={{...styles.postedRow, ...styles.postedHeader}}>
+                <ImageBackground 
+                source={{uri: props.imageUrl}} 
+                style={styles.bgImage}
+                >
+                    <View style={styles.titleContainer}>
+                        <Text style={styles.title} numberOfLines={1}>{props.title}</Text>
+                    </View>
+                </ImageBackground>
+
+            </View>
+            <View style={{...styles.postedRow, ...styles.postedDetail}}>
+                <Text >Price: {props.price} euro</Text>
+                <Text >Condition: {props.condition}</Text>
+                <Text >{props.description}</Text>
+                <View style={styles.iconsContainer}>
+                <IconButton
+                    iconName="magnifying-glass"
+                    icon="circle-with-cross"
+                    style={styles.iconButton}
+                ></IconButton>
+                <IconButton
+                    iconName="magnifying-glass"
+                    icon="pencil"
+                    style={styles.iconButton1}
+                ></IconButton>
+                </View>
+            </View>
+        </View>
+    </TouchableOpacity>
+    </View>
+)
+};
+
+const styles = StyleSheet.create({
+    postedItem:{
+        height:350,
+        width:'100%',
+        borderWidth:0,
+        borderRadius:5,
+        overflow: 'hidden',
+        backgroundColor: '#C5CBE3',
+        padding: 10,
+        marginVertical:10,
+    },
+    titleContainer:{
+        backgroundColor:'rgba(0,0,0,0.5)',
+        paddingVertical:5,
+        paddingHorizontal:20,
+    },
+    title:{
+        fontSize:20,
+        color: 'white',
+
+        textAlign:'center',
+
+    },
+    postedRow:{
+    },
+    postedHeader:{
+        height:'60%',
+    },
+    postedDetail:{
+        height:'40%',
+        paddingHorizontal:10,
+        justifyContent:'space-between',
+        alignItems:'center',
+
+    },
+    bgImage:{
+        width:'100%',
+        height:'100%',
+        justifyContent:'flex-end'
+
+    },
+    iconsContainer:{
+        flexDirection:'row',
+        justifyContent:'flex-end',
+    },
+    iconButton1:{
+        margin:10,  
+    },
+    iconButton:{
+        margin:10,
+    }
+});
+
+
+
+
+
+
+
+    // container:{
+    //     flex:1,
+    //     padding: 15,
+    //     shadowOpacity: 0.26,
+    //     shadowOffset: {width:0 , height:2},
+    //     shadowRadius: 10,
+    //     elevation:3,
+    //     justifyContent: 'flex-end',
+    //     alignItems:'flex-end',
+    //     borderRadius:5,
+    // },
+    // title:{
+    //     fontFamily: 'open-sans-bold',
+    //     fontSize: 22,
+    //     textAlign:'right',
+    // }
+
+
+
+export default MyItemCard;
--- src/screens/ListItemsScreen.js
+++ src/screens/ListItemsScreen.js
@@ -1,14 +1,51 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Text } from "react-native";
-import HeaderComponent from "../components/HeaderComponent";
+import { StyleSheet, View, Text, FlatList } from "react-native";
+
+import { POSTEDITEMS } from "../data/dummy-data";
+import MyItemCard from "../components/MyItemCard";
 import EditableCard from "../components/EditableCard";
+import HeaderComponent from "../components/HeaderComponent";
+
 
 function ListItemsScreen(props) {
+
+  
+  // created By Hossein to read data from dummy-data
+  // const myItemsList = POSTEDITEMS.find(posted => posted.customerId === 'p004');
+  const myItemsList = POSTEDITEMS.filter(
+        posted => posted.customerId.indexOf('p004') >= 0);
+
+  // created by Hossein to render MyItemCard component details*****************
+    const renderMyItem = itemData =>{
+        console.log(myItemsList);
+        return( 
+            <MyItemCard 
+            title={itemData.item.title} 
+            price={itemData.item.price}
+            condition={itemData.item.condition}            
+            description={itemData.item.description}
+            onSelectPost={()=>{}}
+            imageUrl={itemData.item.imageUrl}
+            />
+          );
+    };
+
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>My Items...</Text>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>My Items...</Text>
+      </View>
       <View style={styles.rect}>
-        <EditableCard style={styles.editableCard}></EditableCard>
+        {/* <EditableCard style={styles.editableCard}></EditableCard> */}
+
+        <FlatList
+        data={myItemsList}
+        keyExtractor={(item, index)=> item.id}
+        renderItem={renderMyItem}
+        style={{width:'80%', }}
+        />
+
       </View>
       <View style={styles.headerContainer}>
         <HeaderComponent style={styles.header}></HeaderComponent>
@@ -21,27 +58,37 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  header: {
-    height: 39,
-    width: 339,
-    marginTop: 35,
-    marginLeft: 10
+  titleContainer:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
   },
   title: {
     //fontFamily: "roboto-regular",
     color: "#121212",
-    marginTop: 30,
-    marginLeft: 26
+    margin:20,
+    fontSize: 20,
+
+  },
+  headerContainer:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+
+  },
+  header: {
+    height: 60,
+    width: '100%',
   },
   rect: {
-    width: 296,
-    height: 254,
-    marginTop: 9,
-    marginLeft: 32
+    flex:8,
+    width: '100%',
+    justifyContent:'flex-start',
+    alignItems:'center',
+    margin:10,
   },
   editableCard: {
-    height: 289,
-    width: 296
+    width: '80%',
   }
 });
 
