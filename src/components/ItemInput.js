d8bfddad4232862122a40634cc9bbbc25b08a062
Coder-AD-3000 <78084889+Coder-AD-3000@users.noreply.github.com>
2021-10-08 19:23:27 +0300

    AD - working on the Create Item - backend functionality
    AD - working on the Create Item - backend functionality

---
+++ src/components/ItemInput.js
@@ -0,0 +1,98 @@
+import React, {useState} from 'react';
+import {StyleSheet, View, TextInput, Button, Modal, findNodeHandle} from 'react-native';
+
+const ItemInput=(props)=>{
+    const data = "This is data from Child Component to the Parent Component. :)"
+    const [name, setName]=useState('');
+    const [price, setPrice]=useState(0);
+
+    // State variable
+    const [item, setItem] = useState({
+        name: 'some name',
+        price: 0,
+        description: 'some description',
+        category: 'some category',
+    });
+    
+    const nameInputHandler=(enteredText)=>{
+        item.name = enteredText;
+        console.log('entered text/name: ' + enteredText);
+    }
+
+    const priceInputHandler=(enteredText)=>{
+        item.price = enteredText;
+        console.log('entered text/price: ' + enteredText);
+    }
+
+    const descriptionInputHandler=(enteredText)=>{
+        item.description = enteredText;
+        console.log('entered text/description: ' + enteredText);
+    }
+
+    const categoryInputHandler=(enteredText)=>{
+        item.category = enteredText;
+        console.log('entered text/category: ' + enteredText);
+    }
+
+    // For Controlling modal
+    const addItem=()=>{
+        props.onAddItem(item);
+    }
+    const cancelItem=()=>{
+        props.onCancelItem();
+    }
+
+    // Return
+    return (
+        <Modal visible={props.visibility} animationType="slide">
+            <View style={styles.formStyle}>
+                <TextInput placeholder="Item's name" 
+                    style={styles.inputStyle} 
+                    onChangeText={nameInputHandler}/>
+                <TextInput placeholder="Item's price" 
+                    style={styles.inputStyle} 
+                    onChangeText={priceInputHandler}/>
+                <TextInput placeholder="Item's description" 
+                    style={styles.inputStyle} 
+                    onChangeText={descriptionInputHandler}/>
+                <TextInput placeholder="Item's category" 
+                    style={styles.inputStyle} 
+                    onChangeText={categoryInputHandler}/>    
+                <View style={styles.buttonView}>
+                    <View style={styles.button}>
+                    <Button color='red' title="Cancel" onPress={cancelItem}/>
+                    </View>
+                    <View style={styles.button}>
+                    <Button color='green' title="Add" onPress={addItem}/>
+                    </View>
+                </View>
+            </View>
+        </Modal>
+    );
+}
+
+const styles=StyleSheet.create({
+    formStyle: {
+        flex:1,
+        flexDirection: 'column',
+        justifyContent:'center',
+        alignItems:"center"
+      },
+      inputStyle: {
+        borderWidth: 2, 
+        borderColor: 'red', 
+        padding: 10,
+        width:'80%',
+        marginBottom:10,
+      },
+      buttonView:{
+        width:'60%',
+        flexDirection: 'row',
+        justifyContent:"space-around",
+      },
+      button:{
+        width:'40%',
+      }
+});
+
+export default ItemInput;
--- src/screens/CreateItemScreen.js
+++ src/screens/CreateItemScreen.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
-import { StyleSheet, View, Text, ScrollView } from "react-native";
+import React, { Component, useState, useEffect } from "react";
+import { StyleSheet, View, Text, FlatList, 
+  ScrollView, ActivityIndicator, Button, ProgressViewIOSComponent } from "react-native";
 import HeaderComponent from "../components/HeaderComponent";
 import ImageUploader from "../components/ImageUploader";
 import CreateItemForm from "../components/CreateItemForm";
@@ -10,7 +11,310 @@ import TextStyling from '../constants/fontstyling'
 import { Margins, Paddings } from "../constants/constvalues";
 import colors from "../constants/colors";
 
+// AD - for testing only (might delete later)
+import ListItem from "../components/ListItem";
+import ItemInput from "../components/ItemInput";
+
 function CreateItemScreen(props) {
+
+  //
+  //
+  //
+  // functions related to the input field functionality
+
+  const [hasMessage, setMessage] = useState(false);
+  const [messageDisplayed, setMessageDisplayed] = useState('');
+  const [items, setItems] = useState([]);
+  const [itemList, addItemToList] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [isVisible, setVisibility] = useState(false);
+
+  // Custom Functions ****************************************************************************************
+  const onAddItem = (childdata) => {
+    addItemToList(itemList =>[...itemList, childdata]);
+    console.log('childdata from child onAddItem App.js: ' + childdata.name);
+    console.log('childdata from child onAddItem App.js: ' + childdata.price);
+    console.log('childdata from child onAddItem App.js: ' + childdata.description);
+    console.log('childdata from child onAddItem App.js: ' + childdata.category);
+    addData(childdata.name, childdata.price, childdata.description, childdata.category);
+    setVisibility(false);
+    //setLoading(true);
+  }
+
+  const cancelAddItem=()=>{
+    setVisibility(false);
+    setLoading(false);
+  }
+
+  const onDeleteItem=(idParam)=>{
+    console.log('idParam: ' + idParam);
+    //setLoading(true);
+    deleteData(idParam)
+  }
+
+  function showError(error){
+    setMessage(true);
+    setMessageDisplayed("Error: " + error);
+    console.log(messageDisplayed);
+  }
+
+  function showConfirmation(message){
+    setMessageDisplayed("Confirmation: " + message);
+    setMessage(true);
+  }
+
+  function closeMessage() {
+    setMessage(false);
+    setLoading(true);
+  }
+
+  // Service Functions ****************************************************************************************
+  // *** GET ***
+  async function fetchData() {
+    //Variable res is used later, so it must be introduced before try block and so cannot be const.
+    let response = null;
+    try{
+      //This will wait the fetch to be done - it is also timeout which might be a response (server timeouts)
+      response = await fetch("http://10.0.2.2:8080/rest/itemservice/getall");
+    }
+    catch(error){
+      showError(error);
+    }
+    try{
+      //Getting json from the response
+      let responseData = await response.json();
+      console.log(responseData);//Just for checking.....
+      setItems(responseData);
+    }
+    catch(error){
+      showError(error);
+    }
+  }
+
+  // *** POST ***
+  async function addData(nameParam, priceParam, descrParam, categoryParam) {
+    console.log('started: async function addData(nameParam, priceParam, descrParam, categoryParam) {');
+    let response = null;
+    let requestOptions = {
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify({
+        name: nameParam.toString(), 
+        price: priceParam*1, // *1 -> numbers only
+        description: descrParam.toString(), 
+        category: categoryParam.toString() 
+      })
+    };
+    try {
+      response = await fetch("http://10.0.2.2:8080/rest/itemservice/addjsonitem", requestOptions)
+    } catch (error) {
+      showError(error);
+    }
+    try {
+      let responseData = await response.json();
+      console.log('responseData: ' + responseData);
+      showConfirmation("Item was successfully added!")
+    } catch (error) {
+      showError(error);
+    }
+  }
+
+  // *** PUT ***
+  // Object props are hardcoded, no input form is available. Works the same way as adding item
+  async function updateData(/*idParam, nameParam, priceParam, descrParam, categoryParam*/) {
+    console.log('started: async function addData(nameParam, priceParam, descrParam, categoryParam) {');
+    let response = null;
+    let requestOptions = {
+      method:'PUT',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify({
+        id: 1,
+        name: "update", 
+        price: 1,
+        description: "update", 
+        category: "update" 
+      })
+    };
+/* 
+    let requestOptions = {
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify({
+        id: idParam*1, // *1 -> numbers only
+        name: nameParam.toString(), 
+        price: priceParam*1, // *1 -> numbers only
+        description: descrParam.toString(), 
+        category: categoryParam.toString() 
+      })
+    };
+ */
+    try {
+      response = await fetch("http://10.0.2.2:8080/rest/itemservice/updatejsonitem", requestOptions)
+    } catch (error) {
+      showError(error);
+    }
+    try {
+      let responseData = await response.json();
+      console.log('responseData: ' + responseData);
+      showConfirmation("Item was successfully added!")
+    } catch (error) {
+      showError(error);
+    }
+  }
+
+  // *** DELETE ***
+  // Delivers parameter as JSON data 
+  async function deleteData(idParam) {
+    console.log('started:  async function deleteData(idParam) {');
+    let response = null;
+    let requestOptions = {
+      method:'DELETE',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify({
+        id: idParam*1, // *1 -> numbers only
+      })
+    };
+    try {
+      response = await fetch("http://10.0.2.2:8080/rest/itemservice/deletejsonitem", requestOptions)
+    } catch (error) {
+      showError(error);
+    }
+    try {
+      let responseData = await response.json();
+      showConfirmation("Item was successfully removed!")
+      console.log('responseData: ' + responseData);
+    } catch (error) {
+      showError(error);
+    }
+  }
+
+  //Delivers URI @PathParam
+/*   async function deleteData(idParam) {
+    console.log('async function deleteData() {');
+    let response = null;
+    let requestOptions = {
+        method: 'DELETE',
+        headers: { 
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': '',
+            'My-Custom-Header': 'id'
+        }
+    };
+    try {
+      response = await fetch('http://10.0.2.2:8080/rest/itemservice/deleteitem/' + idParam, requestOptions)
+    } catch (error) {
+      showError(error);
+    }   
+    try {
+      let responseData = await response.json();
+      showConfirmation("Item was successfully removed!")
+      console.log('responseData: ' + responseData); // JAVA Service method returnes a boolean
+      if (responseData == false){
+        var error = 'Removal was not verified! Please check DB.';
+        console.log(error);
+        showError(error); 
+      }
+    } catch (error) {
+      showError(error);
+    }
+  } */
+
+
+/*   
+  This is called every time the view is rendered
+  The new calls of fetchData (and others) must be stopped somehow, because in
+  those methods are statevariables set, which cause a new re-render. 
+*/
+  useEffect(() => {
+    console.log('useEffect(() => {'); 
+      if (isLoading==true){
+        fetchData();
+        setLoading(false);
+    }
+  });
+
+  // If the 'fetch' is not ready yet, an activityindicator is shown
+  if (isLoading==true) {
+    console.log('if(isLoading==true) {');
+    return (
+      <View style={{flex: 1, padding: 20, justifyContent:'center'}}>
+        <ActivityIndicator size="large" color="#00ff00" />
+      </View>
+    );
+  }
+  // If error or confirm message needs to be displayed
+  else if(hasMessage){
+    console.log('else if(hasError){');
+    return(
+
+      <ScrollView>
+      <View style={styles.container}>  
+
+      <View style={{flex: 1, padding: 20, justifyContent:'center'}}>
+        <Text>{hasMessage}</Text>
+        <Text>{""+messageDisplayed}</Text>
+        <Button title='close' onPress={()=>closeMessage()}/>
+      </View>
+
+      </View>
+      </ScrollView>
+    );
+  }
+  //Otherwise the list is shown
+  else{
+    console.log('else{');
+    return (
+
+      <ScrollView>
+      <View style={styles.container}>  
+
+      <View style={styles.screen}>
+        <Text>{hasMessage}</Text>
+        <Button color='grey' title='Fetch from DB' onPress={()=>fetchData()} />
+        <Button color='darkorange' title='Update Item' onPress={()=>updateData()} />
+        <Button color='dodgerblue' title='Add new Item' onPress={()=>setVisibility(true)} />
+        {/* AD - was originally ListInput */}
+        <ItemInput 
+          visibility={isVisible} 
+          onAddItem={onAddItem}
+          itemList={items} 
+          onCancelItem={cancelAddItem} 
+        />
+        <FlatList
+          keyExtractor={(item) => item.id.toString()} 
+          data={items}
+          renderItem={itemData => 
+            <ListItem id={itemData.item.id} 
+            name={itemData.item.name}
+            price={itemData.item.price}
+            description={itemData.item.description}
+            category={itemData.item.category}
+            onDelete={()=>onDeleteItem(itemData.item.id)} 
+        />}
+      />
+      </View>
+
+      </View>
+      </ScrollView>
+    );
+  }
+};
+
+  //
+  //
+  //
+
+
+  /* AD - original 
+
   return (
     
       <ScrollView>
@@ -29,6 +333,9 @@ function CreateItemScreen(props) {
         
   );
 }
+
+*/
+
 
 const styles = StyleSheet.create({
   container: {
@@ -73,7 +380,23 @@ const styles = StyleSheet.create({
     backgroundColor: "rgba(11,67,130,1)",
     marginTop: 9,
     backgroundColor: colors.light5, // AD - added color    
+  },
+
+/* AD - stylings related to the list functionality */
+  screen: {
+    marginTop: 10,
+    padding: 60,
+    height: '100%'
+  },
+  listItem:{
+    padding: 10,
+    marginVertical: 10,
+    borderWidth: 2,
+    borderColor: '#0f0',
+    backgroundColor: '#fce',    
   }
+
+
 });
 
 export default CreateItemScreen;
