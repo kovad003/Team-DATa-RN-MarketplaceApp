3781efcc341b666b3b09235812542a45a66657c8
Coder-AD-3000 <78084889+Coder-AD-3000@users.noreply.github.com>
2021-10-10 23:09:48 +0300

    Update ListCreatedItem.js
    AD - working on the MyPostedItems list

--- src/components/ListCreatedItem.js
+++ src/components/ListCreatedItem.js
@@ -10,12 +10,19 @@ const ListCreatedItem=(props)=>{
         <View style={styles.ListOuterContainer}>
         <TouchableOpacity activeOpacity={0.8} onLongPress={props.onDelete}>
             <View style={styles.listItemStyle}>
-                <Image 
-                source={{uri: 'https://www.giantbomb.com/a/uploads/scale_small/46/462814/3221502-8667199912-d2d02.jpg'}} style={styles.image}/>
-                <Text>{props.itemId}&#41; {props.title}</Text>
-                <Text>Price: {props.price}€ </Text>
-                <Text>Description: {props.description}</Text>
-                <Text>Posted on: {props.datePosted}</Text>
+
+                <View style={styles.LeftInnerContainer}>
+                    <Image 
+                    source={{uri: 'https://www.giantbomb.com/a/uploads/scale_small/46/462814/3221502-8667199912-d2d02.jpg'}} style={styles.image}/>
+                </View> 
+
+                <View style={styles.rightInnerContainer}>   
+                    <Text>{props.itemId}&#41; {props.title}</Text>
+                    <Text>Price: {props.price}€ </Text>
+                    <Text>Description: {props.description}</Text>
+                    <Text>Posted on: {props.datePosted}</Text>
+                </View>
+
             </View>
         </TouchableOpacity>
         </View>
@@ -23,23 +30,43 @@ const ListCreatedItem=(props)=>{
 }
 const styles = StyleSheet.create ({
     listItemStyle: {
-      borderWidth: 1, // 1
-      //borderColor: 'blue', 
-      borderColor: '#000080', // #000080, #c83232
+    /* 
+     borderWidth: 1, 
+      borderColor: '#000080', 
       padding: 5,       
       marginVertical:5,
+      backgroundColor:"white",
+    
+    
+    */
+      
+      borderWidth: 1, 
+      borderColor: '#000080', 
+      padding: 5,       
+      marginVertical:5,
+      backgroundColor:"white",
 
-      backgroundColor:"white", // #f0f8ff 
+      //display: "flex",
+      flexDirection: "row",
+
+
+
      },
+
      image: {
         width: 70,
         height: 70,
         borderRadius: 35,
      },
      ListOuterContainer: {
-         marginHorizontal: Margins.midsize,
-         
-     }
+         marginHorizontal: Margins.midsize,         
+     },
+     LeftInnerContainer: {
+        backgroundColor: 'red',
+     },
+     rightInnerContainer: {
+        backgroundColor: 'blue',
+    },
   });
 
   export default ListCreatedItem;
