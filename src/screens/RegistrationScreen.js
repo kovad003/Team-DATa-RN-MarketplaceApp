6c974805852d46f47feeec63e4c24dcf45baf61c
Coder-AD-3000 <78084889+Coder-AD-3000@users.noreply.github.com>
2021-10-22 02:30:34 +0300

    AD - implementing validation into RegistrationScreen
    AD - implementing validation into RegistrationScreen. WIP.

--- src/screens/RegistrationScreen.js
+++ src/screens/RegistrationScreen.js
@@ -22,6 +22,7 @@
  */
 
 /* TJ & AD - Standard imports from both React and React-Native */
+import { isEmpty } from "lodash";
 import React, { Component, useState, useEffect } from "react";
 import {
   Button,
@@ -34,7 +35,8 @@ import {
   TextInput,
   TouchableOpacity,
   Image,
-  Modal
+  Modal,
+  Alert
 } from 'react-native';
 
 /* TJ & AD - External component imports */
@@ -46,6 +48,21 @@ import Logo from "../components/Logo";
 function RegistrationScreen(props) {
 
   /************* TJ & AD - State Variable *************/
+
+  // > AD - color validator
+  const [colorValidator, setColorValidator] = useState(true);
+  const [inputValidator, setInputValidator] = useState({
+    userName: false,
+    firstName: false,
+    lastName: false,
+    // image: false,
+    email: false,
+    password: false,
+    // dateOfBirth: false,
+    phone: false,
+    // imageInputHandler: false,
+    
+  });
 
   // TJ & AD - State variable (defines the customer object)
   const [customer, setCustomer] = useState({
@@ -62,12 +79,52 @@ function RegistrationScreen(props) {
 
  /************* TJ & AD - Custom Functions *************/
 
+ // > AD - ALERT messages ======================================================================================================
+ function validationFailedAlert(){
+  Alert.alert(
+    "Warning!",
+    "Please complete the form!",
+    [ 
+        { 
+        text: "OK",
+        onPress: () => console.log("OK Pressed"), 
+        }, ], { cancelable: false } );        
+}
+
+function handleWrongPriceFormat(){
+  Alert.alert(
+    "Wrong Format!",
+    "Price must be a number.",
+    [ 
+        { 
+        text: "OK",
+        onPress: () => console.log("OK Pressed"), 
+        }, ], { cancelable: false } );        
+}
+
   /* TJ & AD - custom functions to control the modal 
   (other functions are called via props, 
     and the customer object can be added) */
 
 const addCustomer=()=>{
-  props.onAddCustomer(customer);
+  var counter = 0;
+    for (const validationItem of Object.entries(inputValidator)) {
+      //console.log(item[0] + " -> " + item[1])
+      if(validationItem[1] == false){
+        counter++;
+      }
+      else{
+        console.log("Validation is OK!")
+      }
+      console.log(validationItem[0] + " -> " + validationItem[1]);
+      console.log("Mistakes counted so far: " + counter);
+    }
+    if (counter == 0) {
+      console.log('Inserting data to DB...');
+      props.onAddCustomer(customer);
+    } else {
+      validationFailedAlert();
+    } 
 }
 
 const cancelCustomer=()=>{
@@ -79,20 +136,53 @@ const cancelCustomer=()=>{
 const firstNameInputHandler=(enteredText)=>{
   customer.firstName = enteredText;
   console.log('entered text/firstName: ' + enteredText);
+  if(isEmpty(enteredText)){
+    
+    inputValidator.firstName = false;
+    console.log("No text was entered!");
+  }
+  else{        
+    inputValidator.firstName = true;
+    console.log('entered text/description: ' + enteredText);
+  }
 }
 const lastNameInputHandler=(enteredText)=>{
   customer.lastName = enteredText;
   console.log('entered text/lastName: ' + enteredText);
+  if(isEmpty(enteredText)){
+    inputValidator.lastName = false;
+    console.log("No text was entered!");
+  }
+  else{        
+    inputValidator.lastName = true;
+    console.log('entered text/description: ' + enteredText);
+  }
 }
 
 const userNameInputHandler=(enteredText)=>{
   customer.userName = enteredText;
   console.log('entered text/userName: ' + enteredText);
+  if(isEmpty(enteredText)){
+    inputValidator.userName = false;
+    console.log("No text was entered!");
+  }
+  else{        
+    inputValidator.userName = true;
+    console.log('entered text/description: ' + enteredText);
+  }
 }
 
 const passwordInputHandler=(enteredText)=>{
   customer.password = enteredText;
     console.log('entered text/password: ' + enteredText);
+    if(isEmpty(enteredText)){
+      inputValidator.password = false;
+      console.log("No text was entered!");
+    }
+    else{        
+      inputValidator.password = true;
+      console.log('entered text/description: ' + enteredText);
+    }
 }
 
 const dateOfBirthInputHandler=(enteredText)=>{
@@ -103,11 +193,27 @@ const dateOfBirthInputHandler=(enteredText)=>{
 const emailInputHandler=(enteredText)=>{
   customer.email = enteredText;
   console.log('entered text/email: ' + enteredText);
+  if(isEmpty(enteredText)){
+    inputValidator.email = false;
+    console.log("No text was entered!");
+  }
+  else{        
+    inputValidator.email = true;
+    console.log('entered text/description: ' + enteredText);
+  }
 }
 
 const phoneInputHandler=(enteredText)=>{
   customer.phone = enteredText;
   console.log('entered text/phone: ' + enteredText);
+  if(isEmpty(enteredText)){
+    inputValidator.phone = false;
+    console.log("No text was entered!");
+  }
+  else{        
+    inputValidator.phone = true;
+    console.log('entered text/description: ' + enteredText);
+  }
 }
 
 const imageInputHandler=(enteredText)=>{
