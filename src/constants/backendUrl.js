8ac777da304d357d5f1e39abd1979c17140ac7fd
Daniel Kovacs <kovacsdansan@gmail.com>
2021-10-31 13:54:41 +0200

    Changed back to Dev mode (Localhost)
    Changed back to Dev mode (Localhost)

--- src/constants/backendUrl.js
+++ src/constants/backendUrl.js
@@ -3,6 +3,6 @@ export default {
 
 
 
-    //backendAddress: 'http://10.0.2.2:8080', // for local java backend
-    backendAddress: 'https://useddo-project.appspot.com/', // for java backend deployed in google cloud
+    backendAddress: 'http://10.0.2.2:8080', // for local java backend
+    // backendAddress: 'https://useddo-project.appspot.com/', // for java backend deployed in google cloud
 };
